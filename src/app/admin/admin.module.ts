5660a9762211716fb5e32d79b6dac9d644cc9b73
maximsondv <maximsondv@gmail.com>
2020-07-06 00:18:50 +0600

    Dashboard page

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,9 +9,16 @@ import {EditPageComponent} from './edit-page/edit-page.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from '../shared/shared.module';
 import {AuthGuard} from './shared/services/auth.guard';
+import {SearchPipe} from './shared/search.pipe';
 
 @NgModule({
-  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    SearchPipe,
+    EditPageComponent],
   imports: [
     CommonModule,
     FormsModule,
