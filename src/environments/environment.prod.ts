51bf6445f39db2f4b2991891d03489fff5f0a6c1
maximsondv <maximsondv@gmail.com>
2020-07-05 23:13:34 +0600

    Create Post

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyAOr9UKNpzPva-C7aD6aApy1lwf7fcBnd4'
+  apiKey: 'AIzaSyAOr9UKNpzPva-C7aD6aApy1lwf7fcBnd4',
+  fbDbUrl: 'https://angular-blog-ba3f4.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyAOr9UKNpzPva-C7aD6aApy1lwf7fcBnd4'
+  apiKey: 'AIzaSyAOr9UKNpzPva-C7aD6aApy1lwf7fcBnd4',
+  fbDbUrl: 'https://angular-blog-ba3f4.firebaseio.com'
 };
 
 /*
